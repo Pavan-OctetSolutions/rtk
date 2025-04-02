3604e7ea95ab72a2aa281198096874743cb5bff2
Pavan <dev.octetsolutions@outlook.com>
2025-04-02 14:45:46 +0530

    feat :sparkles: Add refetch on focus and reconnect

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -3,6 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "todo",
   tagTypes: ["GetAllTodosTag", "AddTodosTag"],
+  refetchOnFocus: true,
+  refetchOnReconnect: true,
   baseQuery: fetchBaseQuery({
     baseUrl: "https://dummyjson.com",
     prepareHeaders: (headers) => {
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import apiSlice from "./apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath] : apiSlice.reducer
     },
     middleware: (previousMiddleware)=> previousMiddleware().concat(apiSlice.middleware)
-})
+});
+
+setupListeners(store.dispatch);
