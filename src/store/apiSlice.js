56a9d67d3bff4fb87ab289b3293492d70036a899
Pavan <dev.octetsolutions@outlook.com>
2025-04-02 10:23:42 +0530

    feat :sparkles: Add baseQuery feature for fetching

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,20 +1,27 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import axios from "axios";
 
-
 const apiSlice = createApi({
-    endpoints:  function(builder){
-        return{
-            getAllTodos: builder.query({
-                queryFn: async()=>{
-                    const response =await axios.get('https://dummyjson.com/todos');
-                    return response;
-                }
-            })
-        }
-    }
+  baseQuery: async (url) => {
+    const response = await axios.get(url);
+    return response;
+  },
+  endpoints: function (builder) {
+    return {
+      getAllTodos: builder.query({
+        query: () => {
+          return "https://dummyjson.com/todos";
+        },
+      }),
+      getTodo: builder.query({
+        query: (id) => {
+          `https://dummyjson.com/todos/${id}`;
+        },
+      }),
+    };
+  },
 });
 
 export default apiSlice;
-const {useGetAllTodosQuery} = apiSlice;
-export {useGetAllTodosQuery};
+const { useGetAllTodosQuery } = apiSlice;
+export { useGetAllTodosQuery };
